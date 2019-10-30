7747f7b6e2d7968a5a6dbec2826c54cdff974008
Solal <solal.depardieu@epitech.eu>
2019-10-30 13:51:14 +0100

    to norm

--- inf_div.c
+++ inf_div.c
@@ -50,12 +50,22 @@ char single_divisor(char *nb_a, char *nb_b, char *base)
     for (int i = 1; i < my_strlen(base); i++) {
         char c = base[i];
         char chr[] = {c, '\0'};
+        //printf("%s\n", inf_mult(nb_b, chr, base));
         if (my_strcmpp(nb_a, inf_mult(nb_b, chr, base)) < 0)
             return res;
         else
             res = base[i];
     }   
     return res;
+}
+
+char *under_f(char *a, char *nb_a, char *nb_b, int len_b)
+{
+    if (my_strcmpp(nb_a, nb_b) < 0){
+        free(nb_a);
+        nb_a = getstr_from_to(a, 0, len_b);
+    }
+    return nb_a;
 }
 
 char *inf_div(char *a, char *b, char *base)
@@ -66,27 +76,17 @@ char *inf_div(char *a, char *b, char *base)
     int len_b = my_strlen(b);
     char *nb_a = getstr_from_to(a, 0, len_b);
     char *nb_b = getstr_from_to(b, 0, len_b);
-    if (my_strcmpp(nb_a, nb_b) < 0){
-        free(nb_a);
-        nb_a = getstr_from_to(a, 0, len_b);
-    }
+    nb_a = under_f(a, nb_a, nb_b, len_b);
     char *res = malloc(sizeof(char) * len_a);
     char *rst = malloc(sizeof(char) * len_a);
-    for (int i = 0; i < len_a; i++) {
-        res[i] = '\0';
-        rst[i] = '\0';
-    }
+    for (int i = 0; i < len_a; res[i] = '\0', rst[i] = '\0', i++);
     int len_nb_a = my_strlen(nb_a);
     for (int i = 0; i < len_a - len_nb_a + 1; i++) {
-
         char c[] = {single_divisor(nb_a, nb_b, base), '\0'};
         res = my_strcat(res, c);
         rst = inf_sub(nb_a, inf_mult(nb_b, c, base));
         char chr[] = {a[len_nb_a + i], '\0'};
-        if (rst[0] == '0')
-            nb_a = chr;
-        else
-            nb_a = my_strcat(rst, chr);
+        nb_a = (rst[0] == '0') ? chr : my_strcat(rst, chr);
     }
     return my_putstr_l_z(res, 0);
 }
--- inf_mod.c
+++ inf_mod.c
@@ -20,6 +20,15 @@ char *getstr_from_to(char *str, int start, int end);
 
 char single_divisor(char *nb_a, char *nb_b, char *base);
 
+char *under_ff(char *a, char *nb_a, char *nb_b, int len_b)
+{
+    if (my_strcmpp(nb_a, nb_b) < 0){
+        free(nb_a);
+        nb_a = getstr_from_to(a, 0, len_b);
+    }
+    return nb_a;
+}
+
 char *inf_mod(char *a, char *b, char *base)
 {
     if (my_strcmpp(a, b) < 0)
@@ -28,27 +37,17 @@ char *inf_mod(char *a, char *b, char *base)
     int len_b = my_strlen(b);
     char *nb_a = getstr_from_to(a, 0, len_b);
     char *nb_b = getstr_from_to(b, 0, len_b);
-    if (my_strcmpp(nb_a, nb_b) < 0){
-        free(nb_a);
-        nb_a = getstr_from_to(a, 0, len_b);
-    }
+    nb_a = under_ff(a, nb_a, nb_b, len_b);
     char *res = malloc(sizeof(char) * len_a);
     char *rst = malloc(sizeof(char) * len_a);
-    for (int i = 0; i < len_a; i++) {
-        res[i] = '\0';
-        rst[i] = '\0';
-    }
+    for (int i = 0; i < len_a; res[i] = '\0', rst[i] = '\0', i++);
     int len_nb_a = my_strlen(nb_a);
     for (int i = 0; i < len_a - len_nb_a + 1; i++) {
-
         char c[] = {single_divisor(nb_a, nb_b, base), '\0'};
         res = my_strcat(res, c);
         rst = inf_sub(nb_a, inf_mult(nb_b, c, base));
         char chr[] = {a[len_nb_a + i], '\0'};
-        if (rst[0] == '0')
-            nb_a = chr;
-        else
-            nb_a = my_strcat(rst, chr);
+        nb_a = (rst[0] == '0') ? chr : my_strcat(rst, chr);
     }
     return rst;
 }
