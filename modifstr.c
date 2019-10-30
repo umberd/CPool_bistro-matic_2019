1c240e1f2d932293534f42d9e67567810fd84420
Baptiste Godeau <baptiste.godeau@epitech.eu>
2019-10-30 15:55:18 +0100

    base and spec management

--- modifstr.c
+++ modifstr.c
@@ -9,54 +9,80 @@
 #include<stddef.h>
 #include<stdlib.h>
 
-void another_newf(char *str, int i, char *res)
+int char_in_array(char c, char *str);
+
+void my_strxcat(char *res, char c, int x)
 {
-    if (str[i] == '*')
-        my_strcat(res, ")*(");
-    if (str[i] == '/')
-        my_strcat(res, ")/(");
-    if (str[i] == '%')
-        my_strcat(res, ")\%(");
-    if (str[i] == '(')
-        my_strcat(res, "((");
-    if (str[i] == ')')
-        my_strcat(res, "))");
+    for (int i = 0; i < x; i++)
+        my_strcat(res, &c);
 }
 
-void newf(char *str, int i, char *res)
+void another_newf(char *str, int i, char *res, char *spec)
 {
-    if (str[i] == '+') {
-        if (((i == 0) || (str[i - 1] < '0' ||
-            str[i - 1] > '9')) && str[i - 1] != ')')
-            my_strcat(res, "0+");
-        else
-            my_strcat(res, "))+((");
+    if (str[i] == spec[4]) {
+        my_strxcat(res, spec[1], 1);
+        my_strxcat(res, spec[4], 1);
+        my_strxcat(res, spec[0], 1);
     }
-    if (str[i] == '-') {
-        if (((i == 0) || (str[i - 1] < '0' ||
-            str[i - 1] > '9')) && str[i - 1] != ')')
-            my_strcat(res, "0-");
-        else
-            my_strcat(res, "))-((");
+    if (str[i] == spec[5]) {
+        my_strxcat(res, spec[1], 1);
+        my_strxcat(res, spec[5], 1);
+        my_strxcat(res, spec[0], 1);
     }
-    another_newf(str, i , res);
+    if (str[i] == spec[6]) {
+        my_strxcat(res, spec[1], 1);
+        my_strxcat(res, spec[6], 1);
+        my_strxcat(res, spec[0], 1);
+    }
+    if (str[i] == spec[0])
+        my_strxcat(res, spec[0], 2);
+    if (str[i] == spec[1])
+        my_strxcat(res, spec[1], 2);
 }
 
-char *modifstr(char *str)
+void newf(char *str, int i, char *res, char *base, char *spec)
+{
+    if (str[i] == spec[2]) {
+        if (((i == 0) || (!char_in_array(str[i - 1], base))) && str[i - 1] != spec[1]) {
+            my_strxcat(res, base[0], 1);
+            my_strxcat(res, spec[2], 1);
+        }
+        else {
+            my_strxcat(res, spec[1], 2);
+            my_strxcat(res, spec[2], 1);
+            my_strxcat(res, spec[0], 2);
+        }
+    }
+    if (str[i] == spec[3]) {
+        if (((i == 0) || (!char_in_array(str[i - 1], base)) && str[i - 1] != spec[1])) {
+            my_strxcat(res, base[0], 1);
+            my_strxcat(res, spec[3], 1);
+        }
+        else {
+            my_strxcat(res, spec[1], 2);
+            my_strxcat(res, spec[3], 1);
+            my_strxcat(res, spec[0], 2);
+        }
+    }
+    another_newf(str, i , res, spec);
+}
+
+char *modifstr(char *str, char *base, char *spec)
 {
     char *res = malloc(sizeof(char) * my_strlen(str) * 6 );
-    my_strcat(res, "(((");
+    my_strxcat(res, spec[0], 3);
     char *c = malloc(sizeof(char) * 1);
     for (int i = 0; str[i] != 0; i++) {
-        newf(str, i, res);
-        if (str[i] != ' ' && str[i] != '+' && str[i] != '-' &&
-            str[i] != '*' && str[i] != '/' && str[i] != '%' &&
+        newf(str, i, res, base, spec);
+        if (str[i] != ' ' && str[i] != spec[2] && str[i] != spec[3] &&
+            str[i] != spec[4] && str[i] != spec[5] && str[i] != spec[6] &&
             str[i] != '\n') {
                 c[0] = str[i];
                 my_strcat(res, c);
         }
     }
     free(c);
-    my_strcat(res, ")))\n");
+    my_strxcat(res, spec[1], 3);
+    my_strcat(res, "\n");
     return res;
 }
