4e762a335dbb99a895af0fb806e6baf6c92c17ad
Baptiste Godeau <baptiste.godeau@epitech.eu>
2019-10-31 15:10:55 +0100

    trash removal

--- error2.c
+++ error2.c
@@ -11,15 +11,10 @@ int print_error(void);
 
 int check_dupe(char *str)
 {
-    for (int i = 0; i < my_strlen(str); i++) {
-        for (int j = 0; j < my_strlen(str); j++) {
-            if (str[i] == str[j] && i != j) {
-                my_putstr("Problem with -> ");
-                my_putstr(str);
-                my_putchar('\n');
+    for (int i = 0; i < my_strlen(str); i++)
+        for (int j = 0; j < my_strlen(str); j++)
+            if (str[i] == str[j] && i != j)
                 return print_error();
-            }
-        }
-    }
     return 1;
 }
+
