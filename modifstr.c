bf338c89c3212f06dfd034595b5acc013637e229
Baptiste Godeau <baptiste.godeau@epitech.eu>
2019-10-31 16:00:09 +0100

    codinette clean modifstr.c

--- modifstr.c
+++ modifstr.c
@@ -18,33 +18,34 @@ void my_strxcat(char *res, char c, int x)
         res = my_strcat(res, string);
 }
 
+void custom_triple_cat(char *res, char a, char b, char c)
+{
+    my_strxcat(res, a, 1);
+    my_strxcat(res, b, 1);
+    my_strxcat(res, c, 1);
+}
+
 void another_newf(char *str, int i, char *res, char *spec)
 {
-    if (str[i] == spec[4]) {
-        my_strxcat(res, spec[1], 1);
-        my_strxcat(res, spec[4], 1);
-        my_strxcat(res, spec[0], 1);
-    }
-    if (str[i] == spec[5]) {
-        my_strxcat(res, spec[1], 1);
-        my_strxcat(res, spec[5], 1);
-        my_strxcat(res, spec[0], 1);
-    }
-    if (str[i] == spec[6]) {
-        my_strxcat(res, spec[1], 1);
-        my_strxcat(res, spec[6], 1);
-        my_strxcat(res, spec[0], 1);
-    }
+    if (str[i] == spec[4])
+        custom_triple_cat(res, spec[1], spec[4], spec[0]);
+    if (str[i] == spec[5])
+        custom_triple_cat(res, spec[1], spec[5], spec[0]);
+    if (str[i] == spec[6])
+        custom_triple_cat(res, spec[1], spec[6], spec[0]);
     if (str[i] == spec[0])
         my_strxcat(res, spec[0], 2);
     if (str[i] == spec[1])
         my_strxcat(res, spec[1], 2);
 }
 
-void newf(char *str, int i, char *res, char *base, char *spec)
+void newf(char *str, int i, char *res, char **tab)
 {
+    char *base = tab[0];
+    char *spec = tab[1];
     if (str[i] == spec[2]) {
-        if (((i == 0) || !char_in_array(str[i - 1], base)) && str[i - 1] != spec[1]) {
+        if (((i == 0) || !char_in_array(str[i - 1], base))
+        && str[i - 1] != spec[1]) {
             my_strxcat(res, base[0], 1);
             my_strxcat(res, spec[2], 1);
         }
@@ -55,7 +56,8 @@ void newf(char *str, int i, char *res, char *base, char *spec)
         }
     }
     if (str[i] == spec[3]) {
-        if (((i == 0) || !char_in_array(str[i - 1], base)) && str[i - 1] != spec[1]) {
+        if (((i == 0) || !char_in_array(str[i - 1], base))
+        && str[i - 1] != spec[1]) {
             my_strxcat(res, base[0], 1);
             my_strxcat(res, spec[3], 1);
         }
@@ -70,11 +72,14 @@ void newf(char *str, int i, char *res, char *base, char *spec)
 
 char *modifstr(char *str, char *base, char *spec)
 {
-    char *res = malloc(sizeof(char) * my_strlen(str) * 6 );
+    char **tab = malloc(sizeof(char *) * 3);
+    char *res = malloc(sizeof(char) * my_strlen(str) * 6);
     my_strxcat(res, spec[0], 3);
     char *c = malloc(sizeof(char) * 1);
+    tab[0] = base;
+    tab[1] = spec;
     for (int i = 0; str[i] != 0; i++) {
-        newf(str, i, res, base, spec);
+        newf(str, i, res, tab);
         if (str[i] != ' ' && str[i] != spec[2] && str[i] != spec[3] &&
             str[i] != spec[4] && str[i] != spec[5] && str[i] != spec[6] &&
             str[i] != '\n') {
